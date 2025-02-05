97d141130d8b14ccd5938e97e2319aa7f05d8507
Aitor nieto <aitor.nieto3@educa.madrid.org>
2025-02-05 09:19:42 +0100

    CAMBIO LOGOS

--- ComponentesVue/src/App.vue
+++ ComponentesVue/src/App.vue
@@ -34,11 +34,6 @@ function changeSection(section) {
 
 <template>
   <div id="app">
-    <h1 class="main-title">
-      Destino Perfecto
-      <img class="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvSd6-0-X6oKvvOX6OoxNgatOXWbbLBvdVjA&s" alt="Logo" />
-    </h1>
-    
     <Menu v-if="currentSection !== 'login' && currentSection !== 'register'" @navigate="changeSection" />
     
     <div v-if="currentSection === 'login'">
