9196aabd9fd9270310669ef9d6733e3080908519
Eidan Martinez <eidan.martinez1@educa.madrid.org>
2025-03-10 09:16:41 +0100

    copie el firebase de marcos

--- ComponentesVue/src/firebase.js
+++ ComponentesVue/src/firebase.js
@@ -1,12 +1,13 @@
 // firebase.js
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
   apiKey: "AIzaSyB-7pS_s5V1OeIfgWgYBG4UkkWBikgA6yo",
   authDomain: "prueba-firebase-5eacc.firebaseapp.com",
-  databaseURL: "https://prueba-firebase-5eacc-default-rtdb.europe-west1.firebasedatabase.app",
+  databaseURL: "https://prueba-firebase-5eacc-default-rtdb.europe-west1.firebasedatabase.app/",
   projectId: "prueba-firebase-5eacc",
   storageBucket: "prueba-firebase-5eacc.firebasestorage.app",
   messagingSenderId: "504883897787",
@@ -14,6 +15,8 @@ const firebaseConfig = {
   measurementId: "G-KQ7PZ794HR"
 };
 
+// Inicializar Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
 export const auth = getAuth(firebaseApp);
 export const db = getFirestore(firebaseApp);
+export const realtimeDb = getDatabase(firebaseApp);
